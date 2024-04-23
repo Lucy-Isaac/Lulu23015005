99a59e7e785d90fffb36ac6d3d70d872356a762a
Lucy-Isaac <lucyisaac3@gmail.com>
2024-04-23 11:46:14 -0700

    modified files

--- BasicSyntax.py/AreaofaRectangle.py
+++
@@ -1,6 +0,0 @@
-print("Area of a Rectangle")
-
-Length =int(input("Please enter length: "))
-Width =int (input("Please enter width: "))
-Area= Length * Width
-print("The Area of the Rectangle is", Area)
--- BasicSyntax.py/Temperature.py
+++
@@ -1,3 +0,0 @@
-celcuis= int(input("Enter the temperature in celcuis: "))
-fahrenheit=(celcuis*9/5+32)
-print ("Coverted", celcuis, "celcuis into fahrenheit =", fahrenheit, "fahrenheit" ) 
